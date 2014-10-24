2709e3a281b69db996129eaab02a7341c750ef2c
Rob Homan <homan.robertc@gmail.com>
2014-10-23 18:35:47 -0700

    Add new aliases - one pipes markdown output to lynx for convenient md previews, the other two toggle whether or not Finder shows hidden files.

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -9,3 +9,13 @@ alias k='tree -C'
 alias c='clear'
 
 alias path='echo $PATH'
+
+function mdpreview () {
+  markdown $1 | lynx --stdin
+}
+
+# After writing this value, it is important to relaunch Finder.
+# This can be done with the correct kill command, or by doing
+# Alt + Right Click + "Relaunch" on the Finder dock icon.
+alias showFiles='defaults write com.apple.Finder AppleShowAllFiles YES; killall Finder'
+alias hideFiles='defaults write com.apple.Finder AppleShowAllFiles NO; killall Finder'
